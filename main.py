e655febf7e20c2d0f09f482bbfc7d8557088c8c6
0jae0 <mail@0jae0.dev>
2023-05-30 07:33:01 +0900

    난 야매 인간 ㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import requests
 import json
+import datetime
 
 keyfile = open('key.txt', 'r')
 key = keyfile.read()
-print(key)
 keyfile.close()
 
+def checkdate():
+    nowdate = datetime.datetime.now()
+    year = nowdate.year
+    month = nowdate.month
+    month_str = "{:02d}".format(month)
+    ym = str(year) + str(month_str)
+    return ym  # 여기에서 'ym' 값을 반환합니다.
+
 def schedule():
-    url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&AA_YMD=202305&Type=json"
+    ym = checkdate()  # 'ym' 값을 여기에서 받습니다.
+    url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&AA_YMD=" + ym + "&Type=json"
     response = requests.get(url)
-    json_data = json.loads(response.te5xt)
-    print(json_data)
+    json_data = json.loads(response.text)
+    rows = json_data['SchoolSchedule'][1]['row']
+    for row in rows:
+        print(f'{row["AA_YMD"]} : {row["EVENT_NM"]}')
 
 schedule()
