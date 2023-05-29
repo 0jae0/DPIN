cf9725f4dfcedc06c06da850cc01c48016649439
0jae0 <mail@0jae0.dev>
2023-05-30 02:47:47 +0900

    MYAAAOOOOOO

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import requests
+import json
+
 keyfile = open('key.txt', 'r')
 key = keyfile.read()
 print(key)
 keyfile.close()
+
+def schedule():
+    url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&AA_YMD=202305&Type=json"
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    print(json_data)
+
+schedule()
