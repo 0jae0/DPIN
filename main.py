c760fad7267a4b190f0b4fc73ddf7d32fdc0fb0c
0jae0 <mail@0jae0.dev>
2023-05-30 07:34:30 +0900

    난야매인간 ㅠㅠㅠㅠㅠ + 함수명변경

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ keyfile = open('key.txt', 'r')
 key = keyfile.read()
 keyfile.close()
 
-def checkdate():
+def checkym():
     nowdate = datetime.datetime.now()
     year = nowdate.year
     month = nowdate.month
@@ -15,12 +15,13 @@ def checkdate():
     return ym  # 여기에서 'ym' 값을 반환합니다.
 
 def schedule():
-    ym = checkdate()  # 'ym' 값을 여기에서 받습니다.
+    ym = checkym()  # 'ym' 값을 여기에서 받습니다.
     url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&AA_YMD=" + ym + "&Type=json"
     response = requests.get(url)
     json_data = json.loads(response.text)
     rows = json_data['SchoolSchedule'][1]['row']
     for row in rows:
         print(f'{row["AA_YMD"]} : {row["EVENT_NM"]}')
+
 
 schedule()
