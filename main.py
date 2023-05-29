015e725f18701fec92314aff2e0454f9b6f10670
0jae0 <mail@0jae0.dev>
2023-05-30 07:50:35 +0900

    아니 야맨데 이게 왜 되는건데 ;;;;

--- main.py
+++ main.py
@@ -12,7 +12,17 @@ def checkym():
     month = nowdate.month
     month_str = "{:02d}".format(month)
     ym = str(year) + str(month_str)
-    return ym  # 여기에서 'ym' 값을 반환합니다.
+    return ym
+
+def checkymd():
+    nowdate = datetime.datetime.now()
+    year = nowdate.year
+    month = nowdate.month
+    month_str = "{:02d}".format(month)
+    day = nowdate.day
+    day_str = "{:02d}".format(day)
+    ymd = str(year) + str(month_str) + str(day_str)
+    return ymd
 
 def schedule():
     ym = checkym()  # 'ym' 값을 여기에서 받습니다.
@@ -23,5 +33,15 @@ def schedule():
     for row in rows:
         print(f'{row["AA_YMD"]} : {row["EVENT_NM"]}')
 
+def timetable():
+    ymd=checkymd()
+    url = "https://open.neis.go.kr/hub/misTimetable?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&GRADE=3&CLASS_NM=4&ALL_TI_YMD=" + ymd + "&Type=json"
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    rows = json_data['misTimetable'][1]['row']
+    for row in rows:
+        print(f'{row["PERIO"]} : {row["ITRT_CNTNT"]} : {row["GRADE"]}학년 {row["CLASS_NM"]}반')
 
+
+timetable()
 schedule()
