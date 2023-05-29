7629f8f0adbe99be126ddf6c5c5cb36ec53c8d08
0jae0 <mail@0jae0.dev>
2023-05-30 07:09:51 +0900

    MYAAAOOOOOO!!!

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ keyfile.close()
 def schedule():
     url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&AA_YMD=202305&Type=json"
     response = requests.get(url)
-    json_data = json.loads(response.text)
+    json_data = json.loads(response.te5xt)
     print(json_data)
 
 schedule()
