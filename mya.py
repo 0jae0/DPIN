1fcf7e8ebeb12eb2369a92355992ad2e0a1a40ee
0jae0 <mail@0jae0.dev>
2023-05-31 01:57:17 +0900

    학교명을 검색해서 학교 코드를 받아온다

--- mya.py
+++ mya.py
@@ -5,6 +5,15 @@ import datetime
 keyfile = open('key.txt', 'r')
 key = keyfile.read()
 keyfile.close()
+def info(SC_CODE, SC_NAME):
+    # https://open.neis.go.kr/hub/schoolInfo
+    url = "https://open.neis.go.kr/hub/schoolInfo?KEY=" + key + "&ATPT_OFCDC_SC_CODE=" + SC_CODE + "&SCHUL_NM=" + SC_NAME + "&Type=json"
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    rows = json_data['schoolInfo'][1]['row']
+    for row in rows:
+        SD_SCHUL_CODE = row['SD_SCHUL_CODE']
+    return SD_SCHUL_CODE
 
 def checkym():
     nowdate = datetime.datetime.now()
@@ -24,39 +33,40 @@ def checkymd():
     ymd = str(year) + str(month_str) + str(day_str)
     return ymd
 
-def schedule():
+def schedule(SC_CODE, SC_NAME):
+    SD_SCHUL_CODE = info(SC_CODE, SC_NAME)
     result = str("")
     ym = checkym()  # 'ym' 값을 여기에서 받습니다.
-    url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&AA_YMD=" + ym + "&Type=json"
+    url = "https://open.neis.go.kr/hub/SchoolSchedule?KEY=" + key + "&ATPT_OFCDC_SC_CODE=" + SC_CODE + "&SD_SCHUL_CODE="  + SD_SCHUL_CODE +   + "&AA_YMD=" + ym + "&Type=json"
     response = requests.get(url)
     json_data = json.loads(response.text)
     rows = json_data['SchoolSchedule'][1]['row']
     for row in rows:
-        result += f'{row["AA_YMD"]} : {row["EVENT_NM"]}'
+        result += f'{row["AA_YMD"]} : {row["EVENT_NM"]} '
     return result
 
-def timetable():
+def timetable(SC_CODE, SC_NAME):
+    SD_SCHUL_CODE = info(SC_CODE, SC_NAME)
     result = str("")
     ymd=checkymd()
-    url = "https://open.neis.go.kr/hub/misTimetable?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&GRADE=3&CLASS_NM=4&ALL_TI_YMD=" + ymd + "&Type=json"
+    url = "https://open.neis.go.kr/hub/misTimetable?KEY=" + key + "&ATPT_OFCDC_SC_CODE=" + SC_CODE + "&SD_SCHUL_CODE="  + SD_SCHUL_CODE  +   + "&GRADE=3&CLASS_NM=4&ALL_TI_YMD=" + ymd + "&Type=json"
     response = requests.get(url)
     json_data = json.loads(response.text)
     rows = json_data['misTimetable'][1]['row']
     for row in rows:
-        result += f'{row["PERIO"]} : {row["ITRT_CNTNT"]} : {row["GRADE"]}학년 {row["CLASS_NM"]}반'
+        result += f'{row["PERIO"]} : {row["ITRT_CNTNT"]} : {row["GRADE"]}학년 {row["CLASS_NM"]} '
     return result
 
-def meal():
+def meal(SC_CODE, SC_NAME):
+    SD_SCHUL_CODE = info(SC_CODE, SC_NAME)
     result = str("")
     ymd=checkymd()
-    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&MLSV_YMD=" + ymd + "&Type=json"
+    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=" + key + "&ATPT_OFCDC_SC_CODE=" + SC_CODE + "&SD_SCHUL_CODE="  + SD_SCHUL_CODE  +   + "&MLSV_YMD=" + ymd + "&Type=json"
     response = requests.get(url)
     json_data = json.loads(response.text)
     rows = json_data['mealServiceDietInfo'][1]['row']
     for row in rows:
-        result += f'{row["MMEAL_SC_NM"]} : {row["CAL_INFO"]} : {row["DDISH_NM"].replace("<br/>", "").replace("11", "복숭아.").replace("10.", "돼지고기.").replace("12.", "토마토.").replace("13.", "아황산류.").replace("14.", "호두.").replace("15.", "닭고기.").replace("16.", "쇠고기.").replace("17.", "오징어.").replace("18.", "조개류.").replace("1.", "난류.").replace("2.", "우유.").replace("3.", "메밀.").replace("4.", "땅콩.").replace("5.", "대두.").replace("6.", "밀.").replace("7.", "고등어.").replace("8.", "게.").replace("9.", "새우.")}'
+        result += f'{row["MMEAL_SC_NM"]} : {row["CAL_INFO"]} : {row["DDISH_NM"].replace("<br/>", "").replace("11", "복숭아.").replace("10.", "돼지고기.").replace("12.", "토마토.").replace("13.", "아황산류.").replace("14.", "호두.").replace("15.", "닭고기.").replace("16.", "쇠고기.").replace("17.", "오징어.").replace("18.", "조개류.").replace("1.", "난류.").replace("2.", "우유.").replace("3.", "메밀.").replace("4.", "땅콩.").replace("5.", "대두.").replace("6.", "밀.").replace("7.", "고등어.").replace("8.", "게.").replace("9.", "새우.")} '
     return result
 
-timetable()
-schedule()
-meal()
+info()
--- r.py
+++ r.py
@@ -1,18 +1,34 @@
 from flask import *
 import mya
+import re
 
 app = Flask(__name__)
-@app.route('/schedule')
-def sc():
-    return mya.schedule()
-
-@app.route('/timetable')
-def tt():
-    return mya.timetable()
-
-@app.route('/meal')
-def ml():
-    return mya.meal()
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({"error": "Internal Server Error"}), 500
+def validate_input(input_string):
+    if re.match("^[A-Za-z0-9]*$", input_string):
+        return True
+    else:
+        return False
+@app.route('/schedule/<string:SC_CODE>/<string:SC_NAME>')
+def sc(SC_CODE, SC_NAME):
+    if validate_input(SC_CODE) and validate_input(SC_NAME):
+        return mya.schedule(SC_CODE, SC_NAME)
+    else:
+        return "Invalid input"
+@app.route('/timetable/<string:SC_CODE>/<string:SC_NAME>')
+def tt(SC_CODE, SC_NAME):
+    if validate_input(SC_CODE) and validate_input(SC_NAME):
+        return mya.timetable(SC_CODE, SC_NAME)
+    else:
+        return "Invalid input"
+@app.route('/meal/<string:SC_CODE>/<string:SC_NAME>')
+def ml(SC_CODE, SC_NAME):
+    if validate_input(SC_CODE) and validate_input(SC_NAME):
+        return mya.meal(SC_CODE, SC_NAME)
+    else:
+        return "Invalid input"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=32767)
