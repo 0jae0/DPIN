848ede3647b05e5f1dcc9b2566e47b0a346f729e
0jae0 <mail@0jae0.dev>
2023-05-30 22:08:37 +0900

    급식인뽀매이션

--- main.py
+++ main.py
@@ -42,6 +42,16 @@ def timetable():
     for row in rows:
         print(f'{row["PERIO"]} : {row["ITRT_CNTNT"]} : {row["GRADE"]}학년 {row["CLASS_NM"]}반')
 
+def meal():
+    ymd=checkymd()
+    url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=" + key + "&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7261025&MLSV_YMD=" + ymd + "&Type=json"
+    response = requests.get(url)
+    json_data = json.loads(response.text)
+    rows = json_data['mealServiceDietInfo'][1]['row']
+    for row in rows:
+        print(f'{row["MMEAL_SC_NM"]} : {row["CAL_INFO"]} : {row["DDISH_NM"].replace("<br/>", "").replace("11", "복숭아.").replace("10.", "돼지고기.").replace("12.", "토마토.").replace("13.", "아황산류.").replace("14.", "호두.").replace("15.", "닭고기.").replace("16.", "쇠고기.").replace("17.", "오징어.").replace("18.", "조개류.").replace("1.", "난류.").replace("2.", "우유.").replace("3.", "메밀.").replace("4.", "땅콩.").replace("5.", "대두.").replace("6.", "밀.").replace("7.", "고등어.").replace("8.", "게.").replace("9.", "새우.")}')
+
 
 timetable()
 schedule()
+meal()
